8480003b720f82f07d919f16b08c89a975d6b2d5
sdcervi <sdcervi@gmail.com>
2022-04-15 14:52:28 -0700

    Write cards to page
    Programmatically write cards to page from data structure contents

--- js/checklist.js
+++ js/checklist.js
@@ -1,2 +1,16 @@
 const checklist = document.getElementById('checklistContainer');
+let checklistContent = `<div class="grid" data-masonry='{ "itemSelector": ".grid-item", "columnWidth": 300, "gutter": 20, "fitWidth": true }'>`;
 
+for (const listIndex in checklistData) {
+	const listGroup = checklistData[listIndex];
+	checklistContent += `<div class="grid-item" id="${listGroup.id}"><h2>${listGroup.title}</h2>`;
+	for (const itemIndex in listGroup.contents) {
+		const item = listGroup.contents[itemIndex];
+		checklistContent += `<div class="form-check"><input class="form-check-input" type="checkbox" value="" id="${listGroup.id}-${item.id}"><label class="form-check-label" for="${listGroup.id}-${item.id}">${item.title}</label></div>`;
+	}
+	checklistContent += `</div>`;
+}
+
+checklistContent += `</div>`;
+
+checklist.innerHTML = checklistContent;
