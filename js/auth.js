66040fa86ecef10c7101c174c759bfe1ff1a975b
sdcervi <sdcervi@gmail.com>
2022-05-01 11:44:51 -0700

    Set up user accounts

---
+++ js/auth.js
@@ -0,0 +1,49 @@
+const db = firebase.firestore();
+
+// Get user data; if data does not exist, initialize
+function getUserData (user) {
+	const userData = db.collection('userData').doc(user.uid);
+	userData.get().then((doc) => {
+        if (doc.exists) {
+			if (document.location.pathname == '/index.html' || document.location.pathname == '/profile.html' ) {
+				getProfileData (user);
+			} else {
+				return;
+			}
+		} else {
+			userData.set({
+				cards: {}
+			}).then(() => {
+				return;
+			}).catch((error) => {
+				console.error('Error creating user data: ', error);
+			});
+		}
+    }).catch((error) => {
+		console.log('Error getting user data: ', error);
+	});
+}
+
+// Log the user out
+function logout () {
+	firebase.auth().signOut()
+		.then(function() {
+			location.href = 'index.html';
+		})
+		.catch(function(error) {
+			console.log ('Sign out error');
+		});
+}
+
+// Check whether the user is logged in
+firebase.auth().onAuthStateChanged(function(user) {
+	if (user) {
+		document.getElementById('signInButton').remove();
+		document.getElementById('profileDropdown').style.display = 'block';
+		document.getElementById('profileUser').textContent = user.displayName;
+		getUserData(user);
+	} else {
+		document.getElementById('signInButton').style.display = 'block';
+		document.getElementById('profileDropdown').remove();
+	}
+});
